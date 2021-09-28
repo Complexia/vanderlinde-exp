ce8684ecb56b23ec56a5fe4693d32293c143f536
Complexia <complexia701@gmail.com>
2021-09-28 17:59:25 +1000

    commented out a buggy import

--- src/pages/ShadowGalactic.tsx
+++ src/pages/ShadowGalactic.tsx
@@ -1,12 +1,12 @@
 import NavBar from "../components/NavBar";
-import InfoBox from "../components/infoBox";
+//import InfoBox from "../components/infoBox";
 function ShadowGalactic() {
     let content = "Bridge between crypto and fiat. Coming soon..."
     return (
         <div className="App">
             <NavBar />
             <div className="App-header">
-                <InfoBox content = { content } />
+                Coming soon...
             </div>
         </div>
     )
