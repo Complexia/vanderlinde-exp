55ec77aba18ef08dbf5578ebb20e16a3724396a8
Complexia <complexia701@gmail.com>
2021-09-30 11:56:01 +1000

    Updated discord link

--- src/components/DiscordBox.tsx
+++ src/components/DiscordBox.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { FaDiscord } from 'react-icons/fa';
 function DiscordBox() {
     return (
-        <Link to={{ pathname: "https://discord.gg/7aHtnjGs" }} target="_blank">
+        <Link to={{ pathname: "https://discord.gg/T3kmcGSA" }} target="_blank">
         
             <FaDiscord size={350}/>
         </Link>
